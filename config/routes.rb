b3bcee54e9bfe84bbeda32b46e1638662b08c977
Rails udemy course <s_meach@hotmail.com>
2016-01-18 12:02:16 +0000

    add my portfolio file and more gems

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 root 'welcome#index'
+get 'my_portfolio', to: 'users#my_portfolio'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
